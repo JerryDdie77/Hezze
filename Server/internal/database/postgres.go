895e5a6e82ce77f766804b15fca3c8ab6b9dd838
JerryDdie77 <shahterchannel@gmail.com>
2026-03-28 11:28:50 +0300

    feat(db): add database connection pool initialization

---
+++ Server/internal/database/postgres.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("open: %w", err)
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("ping: %w", err)
+	}
+
+	return db, nil
+}
