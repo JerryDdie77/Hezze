79a89fc775be8534c271647a440d432faecaa3ad
BeHuk <denis-nejivl9k@yandex.ru>
2026-03-28 20:03:54 +0300

    comit

---
+++ Front/src/FIcon.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { styled } from '@mui/material/styles';
+
+const H1Div = styled('div')({
+    color:'darkblue',
+})
+
+function FIcon() {
+  return (
+    <H1Div>FIcon</H1Div>
+  )
+}
+
+export default FIcon
